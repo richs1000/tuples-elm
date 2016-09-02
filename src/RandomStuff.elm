d895b208edf556cb94bca6405b8781e5905cc594
richs1000 <rsimps04@nyit.edu>
2016-09-02 09:00:42 -0400

    Randomizes order of distractors

--- src/RandomStuff.elm
+++ src/RandomStuff.elm
@@ -42,3 +42,35 @@ compressList lst =
                         helperFunc os (o :: newLst)
     in
         helperFunc lst []
+
+
+insertIntoList : a -> Int -> List a -> List a
+insertIntoList item index lst =
+    List.append (List.take index lst) (item :: (List.drop index lst))
+
+
+randomizeListOrder : List Int -> List a -> List a
+randomizeListOrder randomValues lst =
+    let
+        helperFunc rVals oldLst newLst =
+            case oldLst of
+                [] ->
+                    newLst
+
+                o :: [] ->
+                    o :: newLst
+
+                o :: os ->
+                    let
+                        lstLen =
+                            List.length oldLst
+
+                        index =
+                            pickOne rVals [0..lstLen] 0
+
+                        newLst' =
+                            insertIntoList o index newLst
+                    in
+                        helperFunc (List.drop 1 rVals) os newLst'
+    in
+        helperFunc randomValues lst []
